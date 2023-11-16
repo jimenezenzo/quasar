fca5860ae7667174c6a98e737e0aaa42a355a134
Enzo Jimenez <enzo-jimenez@hotmail.com>
2023-11-16 14:41:14 -0300

    inicio proyecto

---
+++ src/test/java/com/jimenez/operacion/fuego/quasar/OperacionFuegoDeQuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimenez.operacion.fuego.quasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperacionFuegoDeQuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
